75e4aaf627980d1bced78953ca662d1e093121e7
fabiogallo01 <fabiogallo.001@gmail.com>
2024-03-26 12:21:48 +0100

    table deck n scoreboard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/GamingDeck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/GamingDeck.java
@@ -1,7 +1,19 @@
 package it.polimi.ingsw.model.game;
 import it.polimi.ingsw.model.cards.*;
+
+import java.util.*;
+
 public class GamingDeck {
     private int numCards;
+    ArrayList<Card> deck;
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
 
     public GamingDeck(int numCards) {
         this.numCards = numCards;
@@ -17,9 +29,11 @@ public class GamingDeck {
 
     public void shuffle(){}
 
-    public Card drawTopCard(){
-
-        return (Card);
+    public GamingDeck(ArrayList<Card> deck) {
+        this.deck = deck;
     }
+
+    public Card drawTopCard(){
+     }
 
 }
