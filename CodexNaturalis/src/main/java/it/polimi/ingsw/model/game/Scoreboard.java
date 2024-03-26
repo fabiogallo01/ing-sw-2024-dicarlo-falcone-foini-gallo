633094a42b885a932bc9c6eb29a7bcd1afed51ed
fabiogallo01 <fabiogallo.001@gmail.com>
2024-03-26 13:27:42 +0100

    scoreboard

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/Scoreboard.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model.game;
+import java.util.*;
+
+public class Scoreboard {
+    HashMap<Player, Integer> scores;
+
+    public Scoreboard(HashMap<Player, Integer> scores) {
+
+        this.scores = scores;
+    }
+
+    public Scoreboard(HashMap<Player, Integer> scores) {
+        this.scores = scores;
+    }
+
+    public HashMap<Player, Integer> getScores() {
+        return scores;
+    }
+
+    public void setScores(HashMap<Player, Integer> scores) {
+        this.scores = scores;
+    }
+}
+
