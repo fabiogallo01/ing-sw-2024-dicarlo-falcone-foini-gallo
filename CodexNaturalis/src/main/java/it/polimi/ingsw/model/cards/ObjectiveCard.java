502ccc029d2598e77882898427009d5f2fc6008e
AndreaDiC11 <andreadicarlo10@gmail.com>
2024-03-26 15:52:28 +0100

    Create ObjectiveCard

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/ObjectiveCard.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.cards;
+
+public class ObjectiveCard {
+    private int points;
+    private boolean isSecret;
+    private Object[] object;
+    private Pattern pattern;
+    private Kingdom frontKingdom;
+
+    public ObjectiveCard(boolean side, int p, boolean secret, int[] numObject, Pattern pattern) {
+        this.points = p;
+        this.
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public boolean getHidden() {
+        return isSecret;
+    }
+
+    public Object[] getObject() {
+        return object;
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public Kingdom getFrontKingdom() {
+        return frontKingdom;
+    }
+
+    public int calculatePoints() {
+        return 0;
+        // TO DO
+    }
+}
+
