073171c11cd38d7f1dbedadef85e47c69b10443d
AndreaDiC11 <andreadicarlo10@gmail.com>
2024-03-26 14:31:08 +0100

    Card get and set methods

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Card.java
@@ -12,15 +12,31 @@ public abstract class Card {
         return side;
     }
 
-    /* Constructor of class Card
+    // Constructor of class Card
     public Card(boolean side) {
         this.side = side;
         // Initialize the corner arrays with Corner objects
         for (int i = 0; i < frontCorners.length; i++) {
             frontCorners[i] = new Corner();
+        }
+        for (int i = 0; i < backCorners.length; i++) {
             backCorners[i] = new Corner();
         }
-    }*/
+    }
 
+    public Corner[] getBackCorners() {
+        return backCorners;
+    }
 
+    public Corner[] getFrontCorners() {
+        return frontCorners;
+    }
+
+    public void setBackCorners(Corner[] backCorners) {
+        this.backCorners = backCorners;
+    }
+
+    public void setFrontCorners(Corner[] frontCorners) {
+        this.frontCorners = frontCorners;
+    }
 }
