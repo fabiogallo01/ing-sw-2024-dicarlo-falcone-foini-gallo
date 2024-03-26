f4ffcf3ab6541d097b968a9ddf0303f0a0b8ff98
giacomofalcone <giacomofalcone42@gmail.com>
2024-03-26 15:17:36 +0100

    Class GoldenCard created + constructor + get

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/GoldCard.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.model.cards;
+
+public class GoldCard extends Card {
+    private Kingdom[] resources = new Kingdom[5];
+    private ConditionPoint conditionPoint;
+
+    public GoldCard(boolean side, Kingdom king, int points, Corner[] fC, Corner[] bC, Kingdom[] res, ConditionPoint conP) {
+        super(side, king, points, fC, bC); // Assuming Card is having same constructor
+        this.resources = res;
+        this.conditionPoint = conP;
+    }
+
+    public Kingdom[] getResources() {
+        return resources;
+    }
+
+    public ConditionPoint getConditionPoint() {
+        return conditionPoint;
+    }
+
+    //TODO
+    /*@Override
+    public void printFront() {
+        // Specific implementation of GoldCard
+        System.out.println("Front of GoldCard");
+    }
+
+    @Override
+    public void printBack() {
+        // Specific implementation of GoldCard
+        System.out.println("Back of GoldCard");
+    }*/
+}
+
