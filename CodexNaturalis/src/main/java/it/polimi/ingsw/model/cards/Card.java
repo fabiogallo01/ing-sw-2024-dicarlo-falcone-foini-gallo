fa0c6df78c54aee101e9b4be481d127bc45ba9be
giacomofalcone <giacomofalcone42@gmail.com>
2024-03-26 11:40:12 +0100

    Added attributes front and back corner and the getter of side

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Card.java
@@ -1,5 +1,26 @@
 package it.polimi.ingsw.model.cards;
 
 public class Card {
+    // Private attributes of class Card
     private boolean side;
+    // Private attributes of class Card
+    private Corner[] frontCorners = new Corner[4];
+    private Corner[] backCorners = new Corner[4];
+
+    // Getter of side
+    public boolean getSide() {
+        return side;
+    }
+
+    /* Constructor of class Card
+    public Card(boolean side) {
+        this.side = side;
+        // Initialize the corner arrays with Corner objects
+        for (int i = 0; i < frontCorners.length; i++) {
+            frontCorners[i] = new Corner();
+            backCorners[i] = new Corner();
+        }
+    }*/
+
+
 }
