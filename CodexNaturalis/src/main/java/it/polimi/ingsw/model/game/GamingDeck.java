569914770703a92f8d3043211da9bd47c55e8244
fabiogallo01 <fabiogallo.001@gmail.com>
2024-03-26 11:07:28 +0100

    GamingDeck first push

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/GamingDeck.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.model.game;
+import it.polimi.ingsw.model.cards.Card;
+public class GamingDeck {
+    private int numCards;
+
+    public GamingDeck(int numCards) {
+        this.numCards = numCards;
+    }
+
+    public void setNumCards(int numCards) {
+        this.numCards = numCards;
+    }
+
+    public int getNumCards() {
+        return numCards;
+    }
+
+    public void shuffle(){}
+
+    public Card drawTopCard(){
+
+        return (Card);
+    }
+
+}
