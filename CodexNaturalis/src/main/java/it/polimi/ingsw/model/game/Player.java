7248f95d2fc966b4fcf81bf962690d844b0c9441
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-03-26 12:21:39 +0100

    Implement Color, ObjectiveDeck, Player and PlayerArea class.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/Player.java
@@ -0,0 +1,91 @@
+package it.polimi.ingsw.model.game;
+import it.polimi.ingsw.model.cards.*;
+
+public class Player {
+    // Attributes
+    private final String username;
+    private int score;
+    private boolean isYourTurn;
+    private PlayerArea playerArea;
+    private final Color color;
+    private final ObjectiveCard secretObjective;
+    private Card[] hand;
+
+    // Methods
+    public Player(String username, int score, boolean isYourTurn, PlayerArea playerArea, Color color, Card[] hand){
+        this.username = username;
+        this.score = score;
+        this.isYourTurn = isYourTurn;
+        this.playerArea = playerArea;
+        this.color = color;
+        this.secretObjective = chooseSecretObjective();
+        this.hand = hand;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+    public boolean getIsYourTurn(){
+        return isYourTurn;
+    }
+
+    public void setIsYourTurn(boolean isYourTurn){
+        this.isYourTurn = isYourTurn;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public ObjectiveCard getSecretObjective(){
+        return secretObjective;
+    }
+
+    public Card[] getHand(){
+        return hand;
+    }
+
+    public void setHand(Card[] hand){
+        this.hand = hand;
+    }
+
+    public ObjectiveCard[] drawSecretObjectiveCards(){
+        // TODO
+    }
+    public ObjectiveCard chooseSecretObjective() {
+        // TODO
+    }
+
+    public GamingCard drawGamingCardDeck(){
+        // TODO
+    }
+
+    public GamingCard drawGamingCardTable(int position){
+        // TODO
+    }
+
+    public GamingCard drawGoldCardDeck(){
+        // TODO
+    }
+
+    public GamingCard drawGoldCardTable(int position){
+        // TODO
+    }
+
+    public void playCard(int position){
+        // TODO
+    }
+
+    public void removeCardHand(Card card){
+        // TODO
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/PlayerArea.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.game;
+
+public class PlayerArea {
+    // Attributes
+
+    // Methods
+}
