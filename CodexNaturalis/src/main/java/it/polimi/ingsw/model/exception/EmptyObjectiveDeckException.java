d7e576088b99501699f6910f7fc039e7156b08f5
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 10:56:16 +0200

    Add exception package. Create class EmptyObjectiveDeckException.java

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/EmptyObjectiveDeckException.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.model.exception;
+public class EmptyObjectiveDeckException extends Exception {
+    public EmptyObjectiveDeckException(String message) {
+        super(message);
+    }
+}
