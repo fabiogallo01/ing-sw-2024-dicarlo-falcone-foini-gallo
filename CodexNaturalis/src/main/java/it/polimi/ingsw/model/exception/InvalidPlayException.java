446d84adad7dffbbc9db5f17d777db9dfeca1b4f
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 21:03:54 +0200

    Add InvalidPlayException.java exception. Raised if the player chooses a card and a position which are not playable.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/InvalidPlayException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.exception;
+
+public class InvalidPlayException extends Exception{
+    public InvalidPlayException(String message) {
+        super(message);
+    }
+}
