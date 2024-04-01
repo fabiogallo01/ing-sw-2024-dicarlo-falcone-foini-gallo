76003229b8289af31709902d2d5cdb098472fcf9
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 11:22:37 +0200

    Add InvalidNumCardsPlayerHandException.java

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/InvalidNumCardsPlayerHandException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.exception;
+
+public class InvalidNumCardsPlayerHandException extends Exception{
+    public InvalidNumCardsPlayerHandException(String message) {
+        super(message);
+    }
+}
