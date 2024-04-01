fd81c50ca490b0b4b871955c7b46df55ac39317d
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 10:21:14 +0200

    Implemented methods shuffle, isEmpty, drawTopCard.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/ObjectiveDeck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/ObjectiveDeck.java
@@ -1,17 +1,18 @@
 package it.polimi.ingsw.model.game;
 import it.polimi.ingsw.model.cards.ObjectiveCard;
+import java.util.*;
 
 import java.util.ArrayList;
 
 public class ObjectiveDeck {
     // Attributes
     private int numCards;
-    private ArrayList<ObjectiveCard> objectiveCards;
+    private ArrayList<ObjectiveCard> deck;
 
     // Methods
-    public ObjectiveDeck(int numCards, ObjectiveCard[] objectiveCards){
+    public ObjectiveDeck(int numCards, ArrayList<ObjectiveCard> deck){
         this.numCards = numCards;
-        this.objectiveCards = new ArrayList<ObjectiveCard>(objectiveCards);
+        this.deck = deck;
     }
 
     public int getNumCards(){
@@ -22,23 +23,40 @@ public class ObjectiveDeck {
         this.numCards = numCards;
     }
 
-    public ObjectiveCard[] getObjectiveCards(){
-        return objectiveCards;
+    public ArrayList<ObjectiveCard> getDeck(){
+        return deck;
     }
 
-    public void setObjectiveCards(ObjectiveCard[] objectiveCards){
-        this.objectiveCards = objectiveCards;
+    public void setDeck(ArrayList<ObjectiveCard> deck){
+        this.deck = deck;
     }
 
-    public static ObjectiveCard drawTopCard(){
-        ObjectiveCard top =  getObjectiveCards()[getNumCards()-1];
-
-        setNumCard(getNumCards()-1)
-        return top;
-        */
+    public boolean isEmpty(){
+        return numCards == 0;
     }
 
+    // Method for shuffle the deck
     public static void shuffle(){
+        List<ObjectiveCard> cardsList = new ArrayList<>(deck);
+        Collections.shuffle(cardsList);
+        deck.clear();
+        deck.addAll(cardList);
+    }
 
+    // Method for returning the top card of the deck
+    // It also remove that card from the deck
+    // Throw exception if the deck is empty
+    public ObjectiveCard drawTopCard() throws EmptyObjectiveDeckException{
+        if(this.isEmpty()){
+            throw new EmptyObjectiveDeckException("Objective deck is empty, you can't draw from this deck.");
+        }
+        else{
+            ObjectiveCard top =  deck.get(deck.size() - 1);
+            deck.remove(deck.size() - 1);
+
+            // Decrement numCards
+            numCards--;
+            return top;
+        }
     }
 }
