d8b69d2222c7f029b82d0186a880d23c155ff677
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 21:05:57 +0200

    Add InvalidPlayCardIndexException.java exception. Raised if the position of the card played is not in the correct index.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/InvalidPlayCardIndexException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.exception;
+
+public class InvalidPlayCardIndexException extends Exception{
+    public InvalidPlayCardIndexException(String message) {
+        super(message);
+    }
+}
