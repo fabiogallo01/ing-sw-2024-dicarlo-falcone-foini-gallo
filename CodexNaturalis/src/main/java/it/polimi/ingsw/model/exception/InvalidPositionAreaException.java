c4a157cb73e2489ca0a944d7e647d684b12f308f
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-01 21:08:22 +0200

    Add InvalidPositionAreaException.java exception. Raised if the player plays a card in a position that is not part of the playing area indices.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/InvalidPositionAreaException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.exception;
+
+public class InvalidPositionAreaException extends Exception{
+    public InvalidPositionAreaException(String message) {
+        super(message);
+    }
+}
