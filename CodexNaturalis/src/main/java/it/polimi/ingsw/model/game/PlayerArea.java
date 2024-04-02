68dcff519ea5794cc7909c290f743c5191192b23
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-02 18:10:38 +0200

    Added method countHiddenCorner (to be implemented) and other "little" changes.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/PlayerArea.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/PlayerArea.java
@@ -2,22 +2,22 @@ package it.polimi.ingsw.model.game;
 import it.polimi.ingsw.model.cards.*;
 import java.util.*;
 
-public class PlayerArea {
+public class PlayerArea{
     // Attributes
-    private HashMap<int[][], Boolean> area;
+    private boolean[][] area;
     private ArrayList<Card> cards;
 
     // Methods
-    public PlayerArea(HashMap<int[][], Boolean> area, ArrayList<Card> cards) {
+    public PlayerArea(boolean[][] area, ArrayList<Card> cards) {
         this.area = area;
         this.cards = cards;
     }
 
-    public HashMap<int[][], Boolean> getArea() {
+    public boolean[][] getArea() {
         return area;
     }
 
-    public void setArea(HashMap<int[][], Boolean> area) {
+    public void setArea(boolean[][] area) {
         this.area = area;
     }
 
@@ -31,20 +31,29 @@ public class PlayerArea {
 
     public void addCard(Card card, int[] positionArea){
         // "Add" card to the HashMap
+        // It is guaranteed that teh position is valid.
+        area[positionArea[0]][positionArea[1]] = false; // false = cell has a card inside.
 
         // Add card in the list
         cards.add(card); // Polymorphism
+
+        // Assign the relative position in the game to the card
+        card.setInGamePosition(positionArea);
     }
 
     public int countKingdoms(Kingdom kingdom){
         // TODO
     }
 
-    public int countObject(Object object){
+    public int countObject(GameObject object){
         // TODO
     }
 
     public int countPattern(Pattern pattern){
         // TODO
+    }
+
+    public int countHiddenCorner(){
+
     }
 }
