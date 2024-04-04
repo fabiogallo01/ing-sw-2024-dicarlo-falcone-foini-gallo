b5c660833686499a6a29acea9330613cd2b30e5b
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-04 18:45:09 +0200

    Optimization of some method. Removed attribute numCards.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/ObjectiveDeck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/game/ObjectiveDeck.java
@@ -5,53 +5,31 @@ import java.util.*;
 
 public class ObjectiveDeck {
     // Attributes
-    private int numCards;
     private ArrayList<ObjectiveCard> deck;
 
     // Methods
-    public ObjectiveDeck(int numCards, ArrayList<ObjectiveCard> deck){
-        this.numCards = numCards;
+    public ObjectiveDeck(ArrayList<ObjectiveCard> deck){
         this.deck = deck;
-    }
-
-    public int getNumCards(){
-        return numCards;
-    }
-
-    public void setNumCards(int numCards){
-        this.numCards = numCards;
     }
 
     public ArrayList<ObjectiveCard> getDeck(){
         return deck;
     }
 
-    public boolean isEmpty(){
-        return numCards == 0;
-    }
-
     // Method for shuffle deck
     public void shuffleDeck(){
-        List<ObjectiveCard> cardsList = new ArrayList<>(deck);
-        Collections.shuffle(cardsList);
-        deck.clear();
-        deck.addAll(cardsList);
+        Collections.shuffle(deck);
     }
 
     // Method for returning the top card of the deck
     // It also removes that card from the deck
     // Throw exception if the deck is empty
     public ObjectiveCard drawTopCard() throws EmptyObjectiveDeckException{
-        if(this.isEmpty()){
+        if(deck.isEmpty()){
             throw new EmptyObjectiveDeckException("Objective deck is empty, you can't draw from this deck.");
         }
         else{
-            ObjectiveCard top =  deck.get(deck.size() - 1);
-            deck.remove(deck.size() - 1);
-
-            // Decrement numCards
-            numCards--;
-            return top;
+            return deck.removeLast();
         }
     }
 }
