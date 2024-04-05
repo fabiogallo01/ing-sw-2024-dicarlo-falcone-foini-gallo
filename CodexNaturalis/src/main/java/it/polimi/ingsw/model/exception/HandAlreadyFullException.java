125998664e044c2640338495e170e524756fa74d
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-06 01:00:14 +0200

    Add new types of exceptions.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/HandAlreadyFullException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.model.exception;
+
+public class HandAlreadyFullException extends Exception{
+    public HandAlreadyFullException(String message) {super(message);}
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exception/InvalidDrawFromTableException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.model.exception;
+
+public class InvalidDrawFromTableException extends Exception{
+    public InvalidDrawFromTableException(String message) {super(message);}
+}
