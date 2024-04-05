4107994b6c3a6b23f6205bdf19947eec345a9d3b
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-06 01:00:57 +0200

    Add pattern enumeration values.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Pattern.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Pattern.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw.model.cards;
-
 public enum Pattern{
+    PRIMARYDIAGONAL,
+    SECONDARYDIAGONAL,
+    LOWERLEFT,
+    LOWERRIGHT,
+    UPPERLEFT,
+    UPPERRIGHT,
+    NONE
 }
