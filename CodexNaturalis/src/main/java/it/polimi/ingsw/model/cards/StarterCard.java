5b358c40064ba62e30843c4267c5a83e8b3c58ae
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-19 23:15:26 +0200

    Modify name of attribute and parameter.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StarterCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StarterCard.java
@@ -1,14 +1,14 @@
 package it.polimi.ingsw.model.cards;
 
 public class StarterCard extends Card{
-    private final Kingdom[] backKingdoms;
+    private final Kingdom[] frontKingdoms;
 
-    public StarterCard(boolean side, Corner[] frontCorners, Corner[] backCorners, Kingdom[] backKingdoms) {
-        super(side, frontCorners, backCorners);
-        this.backKingdoms = backKingdoms;
+    public StarterCard(boolean side, Corner[] frontCorners, Corner[] backCorners, Kingdom[] frontKingdoms) {
+        super(side, frontCorners, backCorners); // Using the constructor with backCorners
+        this.frontKingdoms = frontKingdoms;
     }
 
-    public Kingdom[] getBackKingdoms() {
-        return backKingdoms;
+    public Kingdom[] getFrontKingdoms() {
+        return frontKingdoms;
     }
 }
