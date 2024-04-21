e8e9514ad38b4ecb842445ffa5c078b9c4f7cfc7
giacomofalcone <giacomofalcone42@gmail.com>
2024-04-21 20:10:25 +0200

    Added Before and After to GameTableTest

--- CodexNaturalis/src/test/java/it/polimi/ingsw/GameTableTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/GameTableTest.java
@@ -1,4 +1,24 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.game.GameTable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTableTest {
+
+    GameTable gameTable = null;
+
+    @Before
+    public void setUp() {
+        gameTable = new GameTable();
+    }
+
+    @After
+    public void tearDown()
+    {}
+
 }
