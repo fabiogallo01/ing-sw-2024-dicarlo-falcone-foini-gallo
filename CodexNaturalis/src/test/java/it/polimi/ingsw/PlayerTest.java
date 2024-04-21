fb606e4fc76d6f5f5ab67f0b64447de5f9970f10
fabiogallo01 <fabiogallo.001@gmail.com>
2024-04-21 23:52:54 +0200

    Minor change

--- CodexNaturalis/src/test/java/it/polimi/ingsw/PlayerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+//TO DO: test di playCard, calculateObjectivePoint
 public class PlayerTest {
+
+
 }
