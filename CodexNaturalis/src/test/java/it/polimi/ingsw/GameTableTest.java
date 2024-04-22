4bfb65b4a3a53bc73cb308987f9029c94bb04c63
giacomofalcone <giacomofalcone42@gmail.com>
2024-04-22 18:43:19 +0200

    Test of the gameTable initialization

--- CodexNaturalis/src/test/java/it/polimi/ingsw/GameTableTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/GameTableTest.java
@@ -6,31 +6,58 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class GameTableTest {
 
     GameTable gameTable = null;
+    private int numPlayers = 4;
 
+    /**
+     * Initialized GameTable with a numPlayers number of players
+     *
+     * @author giacomofalcone
+     */
     @Before
     public void setUp() {
-        gameTable = new GameTable();
+        //gameTable = new GameTable(numPlayers);
+        try {
+            gameTable = new GameTable(numPlayers);
+        }
+        catch (EmptyDeckException | EmptyObjectiveDeckException exception) {
+            fail("Initialization failed: " + exception.getMessage());
+        }
     }
 
     @After
     public void tearDown()
     {}
 
+    /**
+     * Tests if the gameTable initialization is not null
+     * and if the number of players is correct
+     *
+     * @author giacomofalcone
+     */
     @Test
+    @DisplayName("Test correct initialization of GameTable")
+    void testInitialization() {
+        assertNotNull(gameTable, "GameTable object is not null");
+        assertEquals(numPlayers, gameTable.getNumPlayers(), "Correct number of players");
+    }
+
+    //@Test
     //a method to test that I have created 40 resource cards
 
-    @Test
+
+    //@Test
     //a method to test that I have created 40 gold cards
 
-    @Test
+    //@Test
     //a method to test that I have created 6 starter cards
 
-    @Test
+    //@Test
     //a method to test that I have created 16 objective cards
 
 
