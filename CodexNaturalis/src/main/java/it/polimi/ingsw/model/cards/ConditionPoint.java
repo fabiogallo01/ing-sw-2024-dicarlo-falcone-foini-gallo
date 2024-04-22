5a645f828b1e66c70d2aa91663c6cf8d76da4711
giacomofalcone <giacomofalcone42@gmail.com>
2024-04-22 11:28:59 +0200

    Added JavaDoc to ConditionPoint class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/ConditionPoint.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/ConditionPoint.java
@@ -1,9 +1,35 @@
 package it.polimi.ingsw.model.cards;
 
+/**
+ * Enumeration of different ways to score points with gold cards.
+ *
+ * @author giacomofalcone
+ */
+
 public enum ConditionPoint {
+
+    /**
+     * Get points for every quill on the board
+     */
     QUILL,
+
+    /**
+     * Get points for every inkwell on the board
+     */
     INKWELL,
+
+    /**
+     * Get points for every manuscript on the board
+     */
     MANUSCRIPT,
+
+    /**
+     * Get points for every hidden corner from the played card
+     */
     HIDDENCORNER,
+
+    /**
+     * Get points without conditions
+     */
     NONE
 }
