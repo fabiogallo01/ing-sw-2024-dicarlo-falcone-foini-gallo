37d612bf5034920f9cfa2510499b1baefe32b48f
giacomofalcone <giacomofalcone42@gmail.com>
2024-04-22 12:26:31 +0200

    Added JavaDoc to Kingdom class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Kingdom.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Kingdom.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.model.cards;
 
+/**
+ * Enumeration of different Kingdoms that can be on cards
+ *
+ * @author giacomofalcone, LorenzoFoini
+ */
+
 public enum Kingdom {
     ANIMALKINGDOM,
     PLANTKINGDOM,
