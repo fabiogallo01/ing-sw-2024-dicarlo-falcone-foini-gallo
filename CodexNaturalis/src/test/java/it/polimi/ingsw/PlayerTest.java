b9c059fc26244959a1ce87e9ae00742035143c77
fabiogallo01 <fabiogallo.001@gmail.com>
2024-04-23 20:48:31 +0200

    PlayerTest: changed the setUp in order to make all the tests with the same setUp. All the tests work successfully.

--- CodexNaturalis/src/test/java/it/polimi/ingsw/PlayerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -18,25 +18,27 @@ import java.util.ArrayList;
 
 //TO DO: test di playCard, calculateObjectivePoint
 public class PlayerTest {
-    private Player player, player2;
-    private ObjectiveCard[] commonObjectives;
+    private Player player;
+    private ObjectiveCard[] commonObjectives, commonObjectives2;
 
 
     @Before
     public void setUp() {
-        boolean[][] area = new boolean[81][81];
-        for (int i = 0; i < 81; i++) {
-            for (int j = 0; j < 81; j++) {
-                area[i][j] = true;
-            }
-        }
 
 
-        ObjectiveCard secretObjective = new ObjectiveCard(2, true, new GameObject[]{GameObject.NONE}, Pattern.SECONDARYDIAGONAL, Kingdom.FUNGIKINGDOM);
-        ObjectiveCard commonObjective1 = new ObjectiveCard(2,false, new GameObject[]{GameObject.NONE}, Pattern.NONE, Kingdom.FUNGIKINGDOM);
+        //2 sets of common objectives, to test different cases, only one secret objective
+
+        ObjectiveCard secretObjective = new ObjectiveCard(2, true, new GameObject[]{GameObject.NONE}, Pattern.UPPERRIGHT, Kingdom.ANIMALKINGDOM);
+
+        ObjectiveCard commonObjective3 = new ObjectiveCard(2, false, new GameObject[]{GameObject.NONE}, Pattern.NONE, Kingdom.ANIMALKINGDOM);
+        ObjectiveCard commonObjective4 = new ObjectiveCard(3, false, new GameObject[]{GameObject.QUILL, GameObject.INKWELL, GameObject.MANUSCRIPT}, Pattern.NONE, Kingdom.NONE);
+        commonObjectives = new ObjectiveCard[]{commonObjective3, commonObjective4};
+
+        ObjectiveCard commonObjective1 = new ObjectiveCard(2, false, new GameObject[]{GameObject.NONE}, Pattern.NONE, Kingdom.FUNGIKINGDOM);
         ObjectiveCard commonObjective2 = new ObjectiveCard(3, false, new GameObject[]{GameObject.NONE}, Pattern.LOWERLEFT, Kingdom.PLANTKINGDOM);
-        commonObjectives = new ObjectiveCard[]{commonObjective1, commonObjective2};
+        commonObjectives2 = new ObjectiveCard[]{commonObjective1, commonObjective2};
 
+        //starter card
 
         Corner[] frontCorners = new Corner[]{
                 new Corner(true, false, GameObject.NONE, Kingdom.NONE),
@@ -55,7 +57,9 @@ public class PlayerTest {
         Kingdom[] kingdoms = new Kingdom[]{Kingdom.PLANTKINGDOM, Kingdom.ANIMALKINGDOM, Kingdom.FUNGIKINGDOM};
 
         StarterCard starterCard = new StarterCard(false, frontCorners, backCorners, kingdoms);
-        //StarterCard starterCard2 = new StarterCard(false, frontCorners, backCorners, kingdoms);
+
+        //cards in the hand of the player
+
         ArrayList<GamingCard> hand = new ArrayList<>();
 
         frontCorners = new Corner[]{
@@ -69,34 +73,36 @@ public class PlayerTest {
 
         GoldCard goldCard1 = new GoldCard(true, Kingdom.FUNGIKINGDOM, 1, frontCorners, resources, ConditionPoint.QUILL);
 
-
         hand.add(goldCard1);
+
 
         frontCorners = new Corner[]{
                 new Corner(true, false, GameObject.NONE, Kingdom.NONE),
-                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
-                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
-                new Corner(true, true, GameObject.NONE, Kingdom.NONE)
+                new Corner(true, false, GameObject.INKWELL, Kingdom.NONE),
+                new Corner(true, true, GameObject.NONE, Kingdom.NONE),
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE)
         };
 
-        resources = new Kingdom[]{Kingdom.FUNGIKINGDOM, Kingdom.FUNGIKINGDOM, Kingdom.FUNGIKINGDOM, Kingdom.PLANTKINGDOM};
+        resources = new Kingdom[]{Kingdom.FUNGIKINGDOM, Kingdom.FUNGIKINGDOM, Kingdom.PLANTKINGDOM};
 
-        GoldCard goldCard2 = new GoldCard(true, Kingdom.FUNGIKINGDOM, 2, frontCorners, resources, ConditionPoint.HIDDENCORNER);
-        //GoldCard playedGoldCard = new GoldCard(true, Kingdom.FUNGIKINGDOM, 2, frontCorners, resources, ConditionPoint.HIDDENCORNER);
+        GoldCard goldCard2 = new GoldCard(true, Kingdom.FUNGIKINGDOM, 2, frontCorners, resources, ConditionPoint.INKWELL);
 
         hand.add(goldCard2);
 
+
         frontCorners = new Corner[]{
-                new Corner(true, false, GameObject.NONE, Kingdom.FUNGIKINGDOM),
+                new Corner(true, false, GameObject.NONE, Kingdom.INSECTKINGDOM),
+                new Corner(true, true, GameObject.NONE, Kingdom.NONE),
                 new Corner(true, false, GameObject.NONE, Kingdom.NONE),
-                new Corner(true, false, GameObject.NONE, Kingdom.FUNGIKINGDOM),
-                new Corner(true, true, GameObject.NONE, Kingdom.NONE)
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE)
         };
 
-        GamingCard resourceCard = new GamingCard(true, Kingdom.FUNGIKINGDOM, 0, frontCorners);
-        //GamingCard playedResourceCard3 = new GamingCard(true, Kingdom.FUNGIKINGDOM, 0, frontCorners);
+        GamingCard resourceCard = new GamingCard(true, Kingdom.INSECTKINGDOM, 1, frontCorners);
 
         hand.add(resourceCard);
+
+
+        //cards already played on the player area
 
         frontCorners = new Corner[]{
                 new Corner(true, false, GameObject.NONE, Kingdom.FUNGIKINGDOM),
@@ -117,25 +123,27 @@ public class PlayerTest {
 
         GamingCard playedResourceCard2 = new GamingCard(true, Kingdom.PLANTKINGDOM, 0, frontCorners);
 
+        frontCorners = new Corner[]{
+                new Corner(true, false, GameObject.NONE, Kingdom.FUNGIKINGDOM),
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
+                new Corner(true, false, GameObject.NONE, Kingdom.FUNGIKINGDOM),
+                new Corner(true, true, GameObject.NONE, Kingdom.NONE)
+        };
 
-        ArrayList<Card> cards = new ArrayList<>();
-        PlayerArea playerArea = new PlayerArea(area, cards);
-
-        playerArea.addCard( starterCard, new int[]{40,40});
-        playerArea.addCard( playedResourceCard1, new int[]{39,39});
-        playerArea.addCard( playedResourceCard2, new int[]{39,41});
-
-        player = new Player("Fabio", 0, playerArea, Color.GREEN, secretObjective, starterCard, hand);
+        GamingCard playedResourceCard3 = new GamingCard(true, Kingdom.FUNGIKINGDOM, 0, frontCorners);
 
 
-        //setup for the counter of the points
-        /*
-        boolean[][] area2 = new boolean[81][81];
-        for (int i = 0; i < 81; i++) {
-            for (int j = 0; j < 81; j++) {
-                area[i][j] = true;
-            }
-        }
+        frontCorners = new Corner[]{
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
+                new Corner(true, false, GameObject.NONE, Kingdom.NONE),
+                new Corner(true, true, GameObject.NONE, Kingdom.NONE)
+        };
+
+        resources = new Kingdom[]{Kingdom.FUNGIKINGDOM, Kingdom.FUNGIKINGDOM, Kingdom.FUNGIKINGDOM, Kingdom.PLANTKINGDOM};
+
+        GoldCard playedGoldCard1 = new GoldCard(true, Kingdom.FUNGIKINGDOM, 2, frontCorners, resources, ConditionPoint.HIDDENCORNER);
+
 
         frontCorners = new Corner[]{
                 new Corner(true, false, GameObject.NONE, Kingdom.NONE),
@@ -155,19 +163,33 @@ public class PlayerTest {
 
         GamingCard playedResourceCard5 = new GamingCard(true, Kingdom.INSECTKINGDOM, 0, frontCorners);
 
-        ArrayList<Card> cards2 = new ArrayList<>();
-        PlayerArea playerArea2 = new PlayerArea(area2, cards2);
 
-        playerArea2.addCard( starterCard, new int[]{40,40});
-        playerArea2.addCard( playedResourceCard1, new int[]{39,39});
-        playerArea2.addCard( playedResourceCard2, new int[]{39,41});
-        //playerArea2.addCard( playedResourceCard3, new int[]{38, 40});
-        //playerArea2.addCard( playedGoldCard, new int[]{37, 41});
-        playerArea2.addCard( playedResourceCard4, new int[]{41,41});
-        playerArea2.addCard( playedResourceCard5, new int[]{42,40});
-        //player2 = new Player("Lorenzo", 0, playerArea2, Color.BLUE, secretObjective, starterCard2, hand);
+        //creation of the player area
 
-        */
+        boolean[][] area = new boolean[81][81];
+        for (int i = 0; i < 81; i++) {
+            for (int j = 0; j < 81; j++) {
+                area[i][j] = true;
+            }
+        }
+
+        ArrayList<Card> cards = new ArrayList<>();
+
+        PlayerArea playerArea = new PlayerArea(area, cards);
+
+        playerArea.addCard(starterCard, new int[]{40, 40});
+        playerArea.addCard(playedResourceCard1, new int[]{39, 39});
+        playerArea.addCard(playedResourceCard2, new int[]{39, 41});
+        playerArea.addCard(playedResourceCard3, new int[]{38, 40});
+        playerArea.addCard(playedGoldCard1, new int[]{37, 41});
+        playerArea.addCard(playedResourceCard4, new int[]{41, 41});
+        playerArea.addCard(playedResourceCard5, new int[]{42, 40});
+
+        //player
+
+        player = new Player("Fabio", 0, playerArea, Color.GREEN, secretObjective, starterCard, hand);
+
+
     }
 
 
@@ -179,7 +201,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_occupiedPositionToPlayGiven_ShouldThrowInvalidPlayException_ShouldShowErrorMessage() {
         try {
-            player.playCard(3, new int[]{40, 40}, true);
+            player.playCard(3, new int[]{42, 40}, true);
             fail("All went good.");
         } catch (InvalidPlayException e) {
             assertEquals("You can't play this card in this position. Mistake: There is already a card in that position.", e.getMessage());
@@ -192,7 +214,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_positionWithNoCornersWhereToPlayGiven_ShouldThrowInvalidPlayException_ShouldShowErrorMessage() {
         try {
-            player.playCard(3, new int[]{50, 50}, true);
+            player.playCard(3, new int[]{60, 60}, true);
             fail("All went good.");
         } catch (InvalidPlayException e) {
             assertEquals("You can't play this card in this position. Mistake: There are no cards in the corners of that position.", e.getMessage());
@@ -205,7 +227,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_positionCoveringTwoCornersOfTheSameCardGiven_ShouldThrowInvalidPlayException_ShouldShowErrorMessage() {
         try {
-            player.playCard(3, new int[]{38, 41}, true);
+            player.playCard(3, new int[]{36, 41}, true);
             fail("All went good.");
         } catch (InvalidPlayException e) {
             assertEquals("You can't play this card in this position. Mistake: The card you want to play can't cover two corners of the same card.", e.getMessage()); //DA SISTEMARE IN PLAYER
@@ -218,7 +240,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_goldCardWithNotSatisfiedConditionGiven_ShouldThrowInvalidPlayException_ShouldShowErrorMessage() {
         try {
-            player.playCard(2, new int[]{38, 42}, true);
+            player.playCard(1, new int[]{36, 42}, true);
             fail("All went good.");
         } catch (InvalidPlayException e) {
             assertEquals("You can't play this card in this position. Mistake: There aren't enough resources to place the gold card.", e.getMessage());
@@ -232,7 +254,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_positionCoveringACornerThatCannotBeCoveredGiven_ShouldThrowInvalidPlayException_ShouldShowErrorMessage() {
         try {
-            player.playCard(3, new int[]{40, 42}, true);
+            player.playCard(3, new int[]{40, 38}, true);
             fail("All went good.");
         } catch (InvalidPlayException e) {
             assertEquals("You can't play this card in this position. Mistake: Card covers a corner which can't be covered", e.getMessage());
@@ -273,7 +295,7 @@ public class PlayerTest {
     @Test
     public void playCardTest_normalWorkingPlayGiven() {
         try {
-            player.playCard(3, new int[]{41, 41}, true);
+            player.playCard(2, new int[]{42, 42}, true);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -284,11 +306,11 @@ public class PlayerTest {
 
     @Test
     public void calculateObjectivePointsTest_noPoints() {
-        assertEquals(0,player.calculateObjectivePoints(commonObjectives));
+        assertEquals(0, player.calculateObjectivePoints(commonObjectives));
     }
-/*
+
     @Test
-    public void calculateObjectivePointsTest_points(){
-        assertEquals(7, player2.calculateObjectivePoints(commonObjectives));
-    }*/
+    public void calculateObjectivePointsTest_points() {
+        assertEquals(5, player.calculateObjectivePoints(commonObjectives2));
+    }
 }
