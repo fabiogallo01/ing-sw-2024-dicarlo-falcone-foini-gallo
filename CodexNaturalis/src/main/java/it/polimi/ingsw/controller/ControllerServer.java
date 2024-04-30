5b8b0a9b048cf74a39fd9f0bd1b10703a2df3258
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-04-30 10:36:39 +0200

    Update class, add method for create new player

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/ControllerServer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/ControllerServer.java
@@ -3,8 +3,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Random;
+
+import it.polimi.ingsw.model.cards.*;
 import it.polimi.ingsw.model.exception.*;
 import it.polimi.ingsw.model.game.*;
 
@@ -19,15 +21,18 @@ public class ControllerServer {
     // Assign/create all parameters
     private static final int port = 10000;
     private static int numPlayers = 0;
-    private static final Set<String> playersUsernames = new HashSet<>();
+    private static final ArrayList<String> playersUsernames = new ArrayList<>();
     public static ArrayList<ViewClientHandler> clients= new ArrayList<>();
     private static GameTable gameTable;
+    private static ArrayList<String> colors = new ArrayList<>();
+    public static ArrayList<String> playersColors = new ArrayList<>();
+    public static ArrayList<ObjectiveCard> playersSecretCards = new ArrayList<>();
 
     /**
      * Main method
      * It is necessary for set up the server and accept connections
      * It also initialises and start threads
-     * Catch IOException and exceptions throw by gameTable
+     * Catch IOException
      *
      * @author Lorenzo Foini
      */
@@ -35,6 +40,13 @@ public class ControllerServer {
         // Run server using new server socket
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is running...");
+
+            // Initialise colors
+            colors.add("Blue");
+            colors.add("Green");
+            colors.add("Red");
+            colors.add("Yellow");
+
             // While the number of connection to the server is less than the number of game's players
             // then the server wait for new connection
             while (numPlayers == 0 || playersUsernames.size() < numPlayers) {
@@ -43,25 +55,20 @@ public class ControllerServer {
                 // Using thread for handle connection
                 ViewClientHandler client = new ViewClientHandler(clientSocket);
                 client.start(); // Start thread
-                if(playersUsernames.size() == 1) {
-                    // Initialise gameTable => Moved to ViewClientHandler???
-                    gameTable = new GameTable(numPlayers);
-                }
 
                 // ADD ViewClientHandler to the list of already connected clients
                 clients.add(client);
 
                 // Add player's username to the list of players in gameTable
                 // Need to initialise all player's attribute
-                // TODO: gameTable.addPlayer(new Player(all params));
+                Player newPlayer = createNewPlayer(clients.size());
+                gameTable.addPlayer(newPlayer);
             }
 
             // Call method for handle players' turn and flow of the game
             playGame();
-        } catch (IOException e) {
+        } catch (IOException | EmptyDeckException e) {
             System.err.println("An I/O error occurred: " + e.getMessage());
-        } catch (EmptyObjectiveDeckException | EmptyDeckException e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -122,14 +129,136 @@ public class ControllerServer {
     }
 
     /**
-     * gameTable setter
+     * gameTable initialization
      * It is necessary to use synchronized because of threads
      *
-     * @param newGameTable representing gameTable to be set
+     * @param numPlayers representing number of players
      * @author Lorenzo Foini
      */
-    public static synchronized void setGameTable(GameTable newGameTable) {
-        gameTable = newGameTable;
+    public static synchronized void initialiseGameTable(int numPlayers) {
+        try {
+            gameTable = new GameTable(numPlayers);
+        } catch (EmptyDeckException | EmptyObjectiveDeckException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Colors getter
+     * It is necessary to use synchronized because of threads
+     *
+     * @return players' color
+     * @author Lorenzo Foini
+     */
+    public static synchronized ArrayList<String> getColors() {
+        return colors;
+    }
+
+    /**
+     * Colors setter
+     * It is necessary to use synchronized because of threads
+     *
+     * @param availableColors new list of available colors
+     * @author Lorenzo Foini
+     */
+    public static synchronized void setColors(ArrayList<String> availableColors) {
+        colors = availableColors;
+    }
+
+    /**
+     * players' colors getter
+     * It is necessary to use synchronized because of threads
+     *
+     * @return players' color
+     * @author Lorenzo Foini
+     */
+    public static synchronized ArrayList<String> getPlayersColors() {
+        return playersColors;
+    }
+
+    /**
+     * Add color to the list of players' colors
+     * It is necessary to use synchronized because of threads
+     *
+     * @param color new player's color
+     * @author Lorenzo Foini
+     */
+    public static synchronized void addPlayersColors(String color) {
+        playersColors.add(color);
+    }
+
+    /**
+     * players' secret cards getter
+     * It is necessary to use synchronized because of threads
+     *
+     * @return players' secret card
+     * @author Lorenzo Foini
+     */
+    public static synchronized ArrayList<ObjectiveCard> getPlayersSecretCards() {
+        return playersSecretCards;
+    }
+
+    /**
+     * Add secret card to the list of players' secret cards
+     * It is necessary to use synchronized because of threads
+     *
+     * @param card new player's secret card
+     * @author Lorenzo Foini
+     */
+    public static synchronized void addPlayersSecretCards(ObjectiveCard card) {
+        playersSecretCards.add(card);
+    }
+
+    /**
+     * Method for generate three random cards and add iit in player's hand
+     *
+     * @return player's hand
+     * @author Lorenzo Foini
+     */
+    public static ArrayList<GamingCard> fillHand() throws EmptyDeckException {
+        ArrayList<GamingCard> newHand = new ArrayList<>();
+        // Generate random value for three times
+        // If < 0.5 => Add gaming card
+        // else     => Add gold card
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            double randomNumber = random.nextDouble();
+            if(randomNumber < 0.5){ // Add gaming card
+                GamingCard newCard = (GamingCard) gameTable.getResourceDeck().drawTopCard();
+                newHand.add(newCard);
+            }else{ // Add gold card
+                GoldCard newCard = (GoldCard) gameTable.getResourceDeck().drawTopCard();
+                newHand.add(newCard);
+            }
+        }
+
+        return newHand;
+    }
+
+    /**
+     * Method for create a new player and initialise his params
+     *
+     * @param index representing player's index in list of players
+     * @author Lorenzo Foini
+     */
+    public static Player createNewPlayer(int index) throws EmptyDeckException {
+        String username = playersUsernames.get(index);
+        int score = 0;
+        Color color = Color.valueOf(playersColors.get(index).toUpperCase());
+        ObjectiveCard secretObjective = playersSecretCards.get(index);
+        StarterCard starterCard = (StarterCard) gameTable.getStarterDeck().drawTopCard();
+        ArrayList<GamingCard> hand = new ArrayList<>(fillHand());
+
+        boolean[][] area = new boolean[81][81];
+        for (boolean[] booleans : area) {
+            Arrays.fill(booleans, true);
+        }
+        area[40][40] = false;
+        ArrayList<Card> playedCards = new ArrayList<>();
+        playedCards.add(starterCard);
+        PlayerArea playerArea = new PlayerArea(area, playedCards);
+
+        return new Player(username, score, playerArea, color, secretObjective, starterCard, hand);
     }
 
     /**
