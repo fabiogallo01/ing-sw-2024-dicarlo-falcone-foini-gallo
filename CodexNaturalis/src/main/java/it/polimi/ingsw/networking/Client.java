1d2199c10a9e29a2dc0814a5d328bed00468cc9a
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-05-04 12:14:35 +0200

    Merge all else if statement in one if using or.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 
 /**
  * Class representing client in a client-server architecture
- * It uses socket for establish connection and show/get server's/client's messagea
+ * It uses socket for establish connection and show/get server's/client's messages
  * Use TUI
  *
  * @author Foini Lorenzo
@@ -39,41 +39,27 @@ public class Client {
             String serverMessage;
             while ((serverMessage = in.readLine()) != null) {
                 System.out.println(serverMessage); // Display server message
-                if (serverMessage.startsWith("Specify the number of players (2, 3 or 4):")) {
+
+                // If the message requires an answer from the user, then catch that answer
+                if (serverMessage.startsWith("Specify the number of players (insert 2, 3 or 4):") ||
+                    serverMessage.startsWith("Insert your username:") ||
+                    serverMessage.startsWith("Username already in use. Please insert a new username:") ||
+                    serverMessage.startsWith("Insert your color:") ||
+                    serverMessage.startsWith("Invalid color. Please select a color from the previous list:") ||
+                    serverMessage.startsWith("On which side you want to play the starter card (insert front or back):") ||
+                    serverMessage.startsWith("Insert front or back:") ||
+                    serverMessage.startsWith("Select your secret objective card (insert 1 or 2):") ||
+                    serverMessage.startsWith("Which card you want to play (insert 1, 2 or 3):") ||
+                    serverMessage.startsWith("Please insert 1, 2 or 3:") ||
+                    serverMessage.startsWith("On which side you want to play this card(insert front or back):") ||
+                    serverMessage.startsWith("Insert integer row value:") ||
+                    serverMessage.startsWith("Insert integer column value:")
+                ) {
                     /*
-                      Asks the user to enter the number of players via userInput.readLine()
-                      Sends the response to the server via out.println(numPlayersInput)
+                      Get user's input via userInput.readLine() and pass it to the server via out.println()
                     */
-                    String numPlayersInput = userInput.readLine(); // Get client input in the keyboard
-                    out.println(numPlayersInput); // Pass it to the handler
-                } else if (serverMessage.startsWith("Insert your username:") || serverMessage.startsWith("Username already in use. Please insert a new username:")) {
-                    /*
-                      Asks the user to enter username via userInput.readLine()
-                      Sends the response to the server via out.println(usernameInput)
-                    */
-                    String usernameUsername = userInput.readLine();
-                    out.println(usernameUsername);
-                } else if (serverMessage.startsWith("On which side you want to play the starter card (insert front or back):") || serverMessage.startsWith("Insert front or back")) {
-                    /*
-                      Asks the user to enter the side of starter card via userInput.readLine()
-                      Sends the response to the server via out.println(usernameInput)
-                    */
-                    String sideStarterCardsInput = userInput.readLine();
-                    out.println(sideStarterCardsInput);
-                } else if (serverMessage.startsWith("Insert your color:") || serverMessage.startsWith("Invalid color. Please select a color from the previous list:")) {
-                    /*
-                      Asks the user to enter username via userInput.readLine()
-                      Sends the response to the server via out.println(usernameInput)
-                    */
-                    String usernameColor = userInput.readLine();
-                    out.println(usernameColor);
-                } else if (serverMessage.startsWith("Select your secret objective card (insert 1 or 2)")) {
-                    /*
-                      Asks the user his secret objective card via userInput.readLine()
-                      Sends the response to the server via out.println(usernameInput)
-                    */
-                    String selectedObjectiveCard = userInput.readLine();
-                    out.println(selectedObjectiveCard);
+                    String userInputMessage = userInput.readLine(); // Get client input in the keyboard
+                    out.println(userInputMessage); // Pass it to the handler
                 }
             }
         } catch (IOException e){
