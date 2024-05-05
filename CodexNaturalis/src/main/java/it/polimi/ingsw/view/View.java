e152be83c61c1137c8e32422a109514b732e0058
giacomofalcone <giacomofalcone42@gmail.com>
2024-05-05 20:08:48 +0200

    Added displayStarterCard and printCorner in View

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
@@ -1,14 +1,48 @@
 package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.controller.Controller;
-import it.polimi.ingsw.model.cards.GamingCard;
-import it.polimi.ingsw.model.cards.GoldCard;
-import it.polimi.ingsw.model.cards.ObjectiveCard;
-import it.polimi.ingsw.model.cards.StarterCard;
+import it.polimi.ingsw.model.cards.*;
 
 import java.io.PrintWriter;
 
 public class View {
+
+    public String printCorner(Corner corner){
+        if(corner.getVisible()){
+            if(corner.getEmpty()){
+                return "EMPTY";
+            } else if (corner.getKingdom() != Kingdom.NONE) {
+                if(corner.getKingdom() == Kingdom.ANIMALKINGDOM){
+                    return "ANIMALKINGDOM";
+                }
+                if(corner.getKingdom() == Kingdom.PLANTKINGDOM){
+                    return "PLANTKINGDOM";
+                }
+                if(corner.getKingdom() == Kingdom.INSECTKINGDOM){
+                    return "INSECTKINGDOM";
+                }
+                if(corner.getKingdom() == Kingdom.FUNGIKINGDOM) {
+                    return "FUNGIKINGDOM";
+                }
+            } else {
+                if(corner.getObject() == GameObject.INKWELL){
+                    return "INKWELL";
+                }
+                if(corner.getObject() == GameObject.QUILL){
+                    return "QUILL";
+                }
+                if(corner.getObject() == GameObject.MANUSCRIPT) {
+                    return "MANUSCRIPT";
+                }
+            }
+        }
+        else {
+            return "            ";
+        }
+        return "            ";
+
+    }
+
 
     /**
      * Method to display a given starter card
@@ -17,7 +51,27 @@ public class View {
      * @author Foini Lorenzo
      */
     public synchronized void displayStarterCard(StarterCard starterCard, PrintWriter out){
-        out.println("This is your starter card: " + starterCard.toString());
+        out.println("This is your starter card: \n" +
+                "Front:\n" +
+                "________________________________________________________________________________________\n" +
+                "|" + printCorner(starterCard.getFrontCorners()[0]) + "              " + printCorner(starterCard.getFrontCorners()[1]) +"|\n" +
+                "           ");
+        for (int i=0; i < starterCard.getFrontKingdoms().length; i++) {
+            out.println("                 " + starterCard.getFrontKingdoms()[i] + "   ");
+        }
+        out.println(
+                "\n" +
+                "|" + printCorner(starterCard.getFrontCorners()[2]) + "              " + printCorner(starterCard.getFrontCorners()[3]) +"|\n" +
+                "________________________________________________________________________________________\n" +
+                "\n" +
+                "Back:\n" +
+                "________________________________________________________________________________________\n" +
+                "|" + printCorner(starterCard.getBackCorners()[0]) + "              " + printCorner(starterCard.getBackCorners()[1]) +"|\n" +
+                "\n" +
+                "\n" +
+                "|" + printCorner(starterCard.getBackCorners()[2]) + "              " + printCorner(starterCard.getBackCorners()[3]) +"|\n" +
+                "________________________________________________________________________________________\n"
+                );
     }
 
     /**
