b693f5d92d4877573e79fc900546f5774f66f22e
AndreaDiC11 <andreadicarlo10@gmail.com>
2024-05-05 18:51:46 +0200

    Update View

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
@@ -1,4 +1,53 @@
 package it.polimi.ingsw.view;
 
+import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.model.cards.GamingCard;
+import it.polimi.ingsw.model.cards.GoldCard;
+import it.polimi.ingsw.model.cards.ObjectiveCard;
+import it.polimi.ingsw.model.cards.StarterCard;
+
+import java.io.PrintWriter;
+
 public class View {
+
+    /**
+     * Method to display a given starter card
+     *
+     * @param starterCard given starter card to be displayed
+     * @author Foini Lorenzo
+     */
+    public synchronized void displayStarterCard(StarterCard starterCard, PrintWriter out){
+        out.println("This is your starter card: " + starterCard.toString());
+    }
+
+    /**
+     * Method to display a given resource card
+     *
+     * @param resourceCard given resource card to be displayed
+     * @author Foini Lorenzo
+     */
+    public void displayResourceCard(GamingCard resourceCard, PrintWriter out){
+        out.println("Resource card: " + resourceCard.toString());
+    }
+
+    /**
+     * Method to display a given gold card
+     *
+     * @param goldCard given gold card to be displayed
+     * @author Foini Lorenzo
+     */
+    public void displayGoldCard(GoldCard goldCard, PrintWriter out){
+        out.println("Gold card: " + goldCard.toString());
+    }
+
+    /**
+     * Method to display a given objective card
+     *
+     * @param objectiveCard given objective card to be displayed
+     * @author Foini Lorenzo
+     */
+    public void displayObjectiveCard(ObjectiveCard objectiveCard, PrintWriter out){
+        out.println("Objective card: " + objectiveCard.toString());
+    }
+
 }
