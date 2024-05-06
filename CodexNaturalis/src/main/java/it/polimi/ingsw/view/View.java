19220eca0495773e3f553919d67fa42a22139f88
giacomofalcone <giacomofalcone42@gmail.com>
2024-05-06 17:04:42 +0200

    Added displayObjectiveCard and modified printCorner in View

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/View.java
@@ -7,6 +7,59 @@ import java.io.PrintWriter;
 
 public class View {
 
+    // ANSI escape codes for colors
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+
+    /**
+     * Method to print card's corners
+     *
+     * @param corner is the corner that has to be printed
+     * @author giacomofalcone
+     */
+    public String printCorner(Corner corner){
+        if(corner.getVisible()){
+            if(corner.getEmpty()){
+                return "EMPTY";
+            } else if (corner.getKingdom() != Kingdom.NONE) {
+                if(corner.getKingdom() == Kingdom.ANIMALKINGDOM){
+                    return (ANSI_CYAN + "ANIMALKINGDOM" + ANSI_RESET);
+                }
+                if(corner.getKingdom() == Kingdom.PLANTKINGDOM){
+                    return (ANSI_GREEN + "PLANTKINGDOM" + ANSI_RESET);
+                }
+                if(corner.getKingdom() == Kingdom.INSECTKINGDOM){
+                    return (ANSI_PURPLE + "INSECTKINGDOM" + ANSI_RESET);
+                }
+                if(corner.getKingdom() == Kingdom.FUNGIKINGDOM) {
+                    return (ANSI_RED + "FUNGIKINGDOM" + ANSI_RESET);
+                }
+            } else {
+                if(corner.getObject() == GameObject.INKWELL){
+                    return (ANSI_YELLOW + "INKWELL" + ANSI_RESET);
+                }
+                if(corner.getObject() == GameObject.QUILL){
+                    return (ANSI_YELLOW + "QUILL" + ANSI_RESET);
+                }
+                if(corner.getObject() == GameObject.MANUSCRIPT) {
+                    return (ANSI_YELLOW + "MANUSCRIPT" + ANSI_RESET);
+                }
+            }
+        }
+        else {
+            return "            ";
+        }
+        return "            ";
+
+    }
+
+    /*
     public String printCorner(Corner corner){
         if(corner.getVisible()){
             if(corner.getEmpty()){
@@ -20,14 +73,14 @@ public class View {
         else {
             return "            ";
         }
-    }
+    }*/
 
 
     /**
      * Method to display a given starter card
      *
      * @param starterCard given starter card to be displayed
-     * @author Foini Lorenzo
+     * @author giacomofalcone
      */
     public synchronized void displayStarterCard(StarterCard starterCard, PrintWriter out){
         out.println("This is your starter card: \n" +
@@ -36,7 +89,18 @@ public class View {
                 "|" + printCorner(starterCard.getFrontCorners()[0]) + "              " + printCorner(starterCard.getFrontCorners()[1]) +"|\n" +
                 "           ");
         for (int i=0; i < starterCard.getFrontKingdoms().length; i++) {
-            out.println("        " + starterCard.getFrontKingdoms()[i] + "   ");
+            if (starterCard.getFrontKingdoms()[i] == Kingdom.ANIMALKINGDOM){
+                out.println("             " + ANSI_CYAN + starterCard.getFrontKingdoms()[i] + ANSI_RESET + "   ");
+            }
+            if (starterCard.getFrontKingdoms()[i] == Kingdom.FUNGIKINGDOM){
+                out.println("             " + ANSI_RED + starterCard.getFrontKingdoms()[i] + ANSI_RESET + "   ");
+            }
+            if (starterCard.getFrontKingdoms()[i] == Kingdom.INSECTKINGDOM){
+                out.println("             " + ANSI_PURPLE + starterCard.getFrontKingdoms()[i] + ANSI_RESET + "   ");
+            }
+            if (starterCard.getFrontKingdoms()[i] == Kingdom.PLANTKINGDOM){
+                out.println("             " + ANSI_GREEN + starterCard.getFrontKingdoms()[i] + ANSI_RESET + "   ");
+            }
         }
         out.println(
                 "\n" +
@@ -57,7 +121,7 @@ public class View {
      * Method to display a given resource card
      *
      * @param resourceCard given resource card to be displayed
-     * @author Foini Lorenzo
+     * @author AndreaDiC11
      */
     public void displayResourceCard(GamingCard resourceCard, PrintWriter out){
         out.println("Resource Card: \n" +
@@ -82,7 +146,7 @@ public class View {
      * Method to display a given gold card
      *
      * @param goldCard given gold card to be displayed
-     * @author Foini Lorenzo
+     * @author AndreaDiC11
      */
     public void displayGoldCard(GoldCard goldCard, PrintWriter out){
         out.println("Gold card: \n" +
@@ -112,10 +176,132 @@ public class View {
      * Method to display a given objective card
      *
      * @param objectiveCard given objective card to be displayed
-     * @author Foini Lorenzo
+     * @author giacomofalcone
      */
-    public void displayObjectiveCard(ObjectiveCard objectiveCard, PrintWriter out){
-        out.println("Objective card: " + objectiveCard.toString());
+    public void displayObjectiveCard(ObjectiveCard objectiveCard, PrintWriter out) {
+        out.println("Objective card: \nPoints:" + objectiveCard.getPoints());
+        if (objectiveCard.getFrontKingdom() != Kingdom.NONE) {
+            if(objectiveCard.getPattern() == Pattern.NONE) {
+                if (objectiveCard.getFrontKingdom() == Kingdom.ANIMALKINGDOM) {
+                    out.println(ANSI_CYAN + "ANIMALKINGDOM\nANIMALKINGDOM\nANIMALKINGDOM" + ANSI_RESET);
+                }
+                if (objectiveCard.getFrontKingdom() == Kingdom.PLANTKINGDOM) {
+                    out.println(ANSI_GREEN + "PLANTKINGDOM\nPLANTKINGDOM\nPLANTKINGDOM" + ANSI_RESET);
+                }
+                if (objectiveCard.getFrontKingdom() == Kingdom.INSECTKINGDOM) {
+                    out.println(ANSI_PURPLE + "INSECTKINGDOM\nINSECTKINGDOM\nINSECTKINGDOM" + ANSI_RESET);
+                }
+                if (objectiveCard.getFrontKingdom() == Kingdom.FUNGIKINGDOM) {
+                    out.println(ANSI_RED + "FUNGIKINGDOM\nFUNGIKINGDOM\nFUNGIKINGDOM" + ANSI_RESET);
+                }
+            } else {
+                if (objectiveCard.getPattern() == Pattern.LOWERLEFT) {
+                    out.println(ANSI_GREEN +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" + ANSI_RESET + ANSI_PURPLE +
+                            "------\n" +
+                            "|     |\n" +
+                            "------\n" + ANSI_RESET);
+                } else if (objectiveCard.getPattern() == Pattern.LOWERRIGHT) {
+                    out.println(ANSI_RED +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" + ANSI_RESET + ANSI_GREEN +
+                            "            ------\n" +
+                            "            |     |\n" +
+                            "            ------\n" + ANSI_RESET);
+                } else if (objectiveCard.getPattern() == Pattern.UPPERLEFT) {
+                    out.println(ANSI_CYAN +
+                            "------\n" +
+                            "|     |\n" +
+                            "------\n" + ANSI_RESET + ANSI_PURPLE +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" + ANSI_RESET);
+                } else if (objectiveCard.getPattern() == Pattern.UPPERRIGHT) {
+                    out.println(ANSI_RED +
+                            "             ------\n" +
+                            "             |     |\n" +
+                            "             ------\n" + ANSI_RESET + ANSI_CYAN +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" +
+                            "      ------\n" +
+                            "      |     |\n" +
+                            "      ------\n" + ANSI_RESET);
+                } else if (objectiveCard.getPattern() == Pattern.PRIMARYDIAGONAL) {
+                    if (objectiveCard.getFrontKingdom() == Kingdom.PLANTKINGDOM) {
+                        out.println(ANSI_GREEN +
+                                "------\n" +
+                                "|     |\n" +
+                                "------\n" +
+                                "      ------\n" +
+                                "      |     |\n" +
+                                "      ------\n" +
+                                "             ------\n" +
+                                "             |     |\n" +
+                                "             ------\n" + ANSI_RESET);
+                    } else {
+                        out.println(ANSI_PURPLE +
+                                "------\n" +
+                                "|     |\n" +
+                                "------\n" +
+                                "       ------\n" +
+                                "       |     |\n" +
+                                "       ------\n" +
+                                "              ------\n" +
+                                "              |     |\n" +
+                                "              ------\n" + ANSI_RESET);
+                    }
+                } else if (objectiveCard.getPattern() == Pattern.SECONDARYDIAGONAL) {
+                    if (objectiveCard.getFrontKingdom() == Kingdom.FUNGIKINGDOM) {
+                        out.println(ANSI_RED +
+                                "              ------\n" +
+                                "              |     |\n" +
+                                "              ------\n" +
+                                "       ------\n" +
+                                "       |     |\n" +
+                                "       ------\n" +
+                                "------\n" +
+                                "|     |\n" +
+                                "------\n" + ANSI_RESET);
+                    } else {
+                        out.println(ANSI_CYAN +
+                                "              ------\n" +
+                                "              |     |\n" +
+                                "              ------\n" +
+                                "       ------\n" +
+                                "       |     |\n" +
+                                "       ------\n" +
+                                "------\n" +
+                                "|     |\n" +
+                                "------\n" + ANSI_RESET);
+                    }
+                }
+            }
+        } else { // (objectiveCard.getObjects()[0] != GameObject.NONE)
+            for (int j = 0; j < objectiveCard.getObjects().length; j++) {
+                if (objectiveCard.getObjects()[j] == GameObject.INKWELL) {
+                    out.println(ANSI_YELLOW + "INKWELL" + ANSI_RESET);
+                } else if (objectiveCard.getObjects()[j] == GameObject.QUILL) {
+                    out.println(ANSI_YELLOW + "QUILL" + ANSI_RESET);
+                } else { //if (objectiveCard.getObjects()[j] == GameObject.MANUSCRIPT) {
+                    out.println(ANSI_YELLOW + "MANUSCRIPT" + ANSI_RESET);
+                }
+            }
+        }
+        out.println("\n");
     }
+
 
 }
