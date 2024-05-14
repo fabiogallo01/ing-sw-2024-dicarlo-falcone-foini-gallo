c9799dc1eb6c078108ec4714933791765fd011ee
LorenzoFoini <lorenzo.foini@mail.polimi.it>
2024-05-14 10:14:37 +0200

    Add new method for start TUI or GUI based on player choice

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client.java
@@ -6,11 +6,16 @@ import java.net.Socket;
 /**
  * Class representing client in a client-server architecture
  * It uses socket for establish connection and show/get server's/client's messages
- * Use TUI
+ * args[0] contains UI choice: TUI or GUI (GUI as default if args is empty)
  *
  * @author Foini Lorenzo
  */
 public class Client {
+    private static Socket socket = null; // Initialise socket
+    private static BufferedReader in;
+    private static PrintWriter out;
+    private static BufferedReader userInput;
+    private static boolean useTUI = false; // GUI as default
 
     /**
      * Main method
@@ -22,27 +27,56 @@ public class Client {
         // Initialise server IP and port
         String hostName = "127.0.0.1";
         int port = 54321;
-        Socket socket = null; // Initialise socket
 
         try {
             socket = new Socket(hostName, port);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Input => Server messages to client
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // Output => Client messages to server
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in)); // Read client keyboard
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Input => Server messages to client
+            out = new PrintWriter(socket.getOutputStream(), true); // Output => Client messages to server
+            userInput = new BufferedReader(new InputStreamReader(System.in)); // Read client keyboard
 
-            /*
+            // Check if user plays with TUI or GUI
+            try {
+                String userChoice = args[0].toUpperCase();
+                out.println(userChoice);
+                if (userChoice.equals("TUI")) {
+                    useTUI = true;
+                    startTUI();
+                } else {
+                    startGUI();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                out.println("GUI");
+                startGUI(); // Start GUI as default without command line parameter
+            }
+
+        } catch (IOException e){
+            System.err.println("Game ended because an I/O error occurred: " + e.getMessage());
+        } finally {
+            // Close connection
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private static void startTUI() throws IOException {
+        /*
               The following code it is used for handles communication between the client and the server.
               In particular, the while loop takes care of continuously reading responses from the server via in.readLine(),
               until the server closes the connection or a null message is received.
               For each message received from server, client prints it out to console via System.out.println(serverResponse).
               Iterate until there is a server message.
             */
-            String serverMessage;
-            while ((serverMessage = in.readLine()) != null) {
-                System.out.println(serverMessage); // Display server message
+        String serverMessage;
+        while ((serverMessage = in.readLine()) != null) {
+            System.out.println(serverMessage); // Display server message
 
-                // If the message requires an answer from the user, then catch that answer
-                if (serverMessage.startsWith("Specify the number of players (insert 2, 3 or 4):") ||
+            // If the message requires an answer from the user, then catch that answer
+            if (serverMessage.startsWith("Specify the number of players (insert 2, 3 or 4):") ||
                     serverMessage.startsWith("Insert your username:") ||
                     serverMessage.startsWith("Username already in use. Please insert a new username:") ||
                     serverMessage.startsWith("Insert your color:") ||
@@ -57,25 +91,21 @@ public class Client {
                     serverMessage.startsWith("Insert integer column value:") ||
                     serverMessage.startsWith("Insert 1, 2 or 3:") ||
                     serverMessage.startsWith("Insert the card's number:")
-                ) {
+            ) {
                     /*
                       Get user's input via userInput.readLine() and pass it to the server via out.println()
                     */
-                    String userInputMessage = userInput.readLine(); // Get client input in the keyboard
-                    out.println(userInputMessage); // Pass it to the handler
-                }
-            }
-        } catch (IOException e){
-            System.err.println("Game ended because an I/O error occurred: " + e.getMessage());
-        } finally {
-            // Close connection
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
+                String userInputMessage = userInput.readLine(); // Get client input in the keyboard
+                out.println(userInputMessage); // Pass it to the handler
             }
         }
+    }
+
+    private static void startGUI() throws IOException{
+        // TODO
+    }
+
+    private boolean getUseTui(){
+        return useTUI;
     }
 }
