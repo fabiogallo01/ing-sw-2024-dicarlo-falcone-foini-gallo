aa6ac871120cfe7e2f32449885efa1c7ae63a19e
fabiogallo01 <fabiogallo.001@gmail.com>
2024-05-22 14:11:17 +0200

    Partial implementation of GUI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client2.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/Client2.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.networking;
 
+import it.polimi.ingsw.view.gui.ViewGUI;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,38 +18,61 @@ public class Client2 {
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Connected to the game server");
-
-            String response;
-            while ((response = in.readLine()) != null) {
-                System.out.println(response);
-                //TODO sostituire tutti gli or con una condizione sola, creando una classe di un genere di messaggi
-                if (response.equals("Enter your choice (create/join):") ||
-                        response.equals("Enter number of players (2-4):") ||
-                        response.equals("Specify the number of players (insert 2, 3 or 4):") ||
-                        response.equals("Insert your username:") ||
-                        response.equals("Username already in use. Please insert a new username:") ||
-                        response.equals("Insert your color:") ||
-                        response.equals("Invalid color. Please select a color from the list") ||
-                        response.equals("On which side you want to play the starter card (insert front or back):") ||
-                        response.equals("Insert front or back:") ||
-                        response.equals("Select your secret objective card (insert 1 or 2):") ||
-                        response.equals("Which card you want to play (insert 1, 2 or 3):") ||
-                        response.equals("Please insert 1, 2 or 3:") ||
-                        response.equals("On which side you want to play this card(insert front or back):") ||
-                        response.equals("Insert integer row value:") ||
-                        response.equals("Insert integer column value:") ||
-                        response.equals("Insert 1, 2 or 3:") ||
-                        response.equals("Insert the card's number:") ||
-                        response.equals("Do you want to join this game? (Y/N)")
-                ) {
-                    String input = scanner.nextLine();
-                    out.println(input);
+            try {
+                if (args[0].equalsIgnoreCase("TUI")) {
+                    startTUI(out, in); // Start communication with TUI
+                } else {
+                    startGUI(out, in); // Start communication with GUI
                 }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                startGUI(out, in); // Start GUI as default without command line parameter
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void startTUI(PrintWriter out, BufferedReader in) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Connected to the game server");//TUI
+
+        String response;
+        while ((response = in.readLine()) != null) {
+            System.out.println(response);//TUI
+            //TODO sostituire tutti gli or con una condizione sola, creando una classe di un genere di messaggi
+            if (response.equals("Enter your choice (create/join):") ||
+                    response.equals("Enter number of players (2-4):") ||
+                    response.equals("Specify the number of players (insert 2, 3 or 4):") ||
+                    response.equals("Insert your username:") ||
+                    response.equals("Username already in use. Please insert a new username:") ||
+                    response.equals("Insert your color:") ||
+                    response.equals("Invalid color. Please select a color from the list") ||
+                    response.equals("On which side you want to play the starter card (insert front or back):") ||
+                    response.equals("Insert front or back:") ||
+                    response.equals("Select your secret objective card (insert 1 or 2):") ||
+                    response.equals("Which card you want to play (insert 1, 2 or 3):") ||
+                    response.equals("Please insert 1, 2 or 3:") ||
+                    response.equals("On which side you want to play this card(insert front or back):") ||
+                    response.equals("Insert integer row value:") ||
+                    response.equals("Insert integer column value:") ||
+                    response.equals("Insert 1, 2 or 3:") ||
+                    response.equals("Insert the card's number:") ||
+                    response.equals("Do you want to join this game? (Y/N)")
+            ) {
+                String input = scanner.nextLine();//TUI
+                out.println(input);//TUI
+            }
+        }
+    }
+
+    private static void startGUI(PrintWriter out, BufferedReader in) throws IOException {
+        ViewGUI viewGUI = new ViewGUI();
+        String response;
+        while ((response = in.readLine()) != null) {
+            if (response.equals("Enter number of players (2-4):")){
+                out.println(viewGUI.displayNumberPlayer());
+            }
         }
     }
 }
